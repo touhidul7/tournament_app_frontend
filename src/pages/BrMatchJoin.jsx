aa46d8679ea0af4602a0feee60ecd81522f540a5
subashebrom <subashembrom99@gmail.com>
2025-07-19 18:10:56 +0600

    join page

--- src/pages/BrMatchJoin.jsx
+++ src/pages/BrMatchJoin.jsx
@@ -15,16 +15,29 @@ const BrMatchJoin = () => {
                     </div>
             </div>
             {/* === Join Details === */}
-            <div className="w-full mt-24 justify-items-center">
-                <div className="w-[95%] bg-cardbg">
-                    <h1>Solo Time | Mobile | Regular</h1>
-                    <p>19/07/2025, 02:00 PM</p>
-                    <div>
+            <div className="w-full mt-24 justify-items-center font-Jakarta">
+                <div className="w-[95%] bg-cardbg rounded-md text-white py-2">
+                    <h1 className="text-md font-semibold ml-2">Solo Time | Mobile | Regular</h1>
+                    <p className="text-md text-hoverbg ml-2 mt-1">19/07/2025, 02:00 PM</p>
+                    <div className="flex text-md font-medium ml-2 mt-1 gap-7">
                         <p>Win Prize: <FontAwesomeIcon className="mx-1" icon={faBangladeshiTakaSign}/> 800 Tk</p>
                         <p>Entry Fee: <FontAwesomeIcon icon={faBangladeshiTakaSign}/> 10 TK</p>
                     </div>
-                    <hr />
-                    <p className="font-Siliguri">অবশ্যই এখানে আপনার গেমের নামটি দিয়ে জয়েন করবেন।</p>
+                    <hr className="text-hoverbg my-3" />
+                    <p className="text-lg text-center font-Siliguri text-green-500">অবশ্যই এখানে আপনার গেমের নামটি দিয়ে জয়েন করবেন।</p>
+                    {/* === Join type button === */}
+                    <div className="flex items-center justify-center gap-5 mt-4">
+                        <button className="text-lg font-medium bg-mainbg w-32 py-2 rounded-md inset-shadow-sm inset-shadow-gray-100/70 hover:bg-cardbg hover:inset-shadow-green-500/60 transition-all duration-300 delay-75 ease-in-out" type="submit">Solo</button>
+                        <button className="text-lg font-medium bg-mainbg w-32 py-2 rounded-md inset-shadow-sm inset-shadow-gray-100/70 hover:bg-cardbg hover:inset-shadow-green-500/60 transition-all duration-300 delay-75 ease-in-out" type="submit">Duo</button>
+                        {/* === DropDown Input Text === */}
+                    </div>
+                        <div className="solo mx-2 mt-6">
+                            <input className="w-full bg-cardbg inset-shadow-sm inset-shadow-gray-100/70 px-3 py-3 text-lg font-medium rounded-md focus:outline-none hover:inset-shadow-green-500/60 transition-all duration-300 delay-75 ease-in-out" type="text" placeholder="Player 1 Name" />
+                        </div>
+                        <div className="Duo flex flex-col gap-4 mx-2 mt-6">
+                            <input className="w-full bg-cardbg inset-shadow-sm inset-shadow-gray-100/70 px-3 py-3 text-lg font-medium rounded-md focus:outline-none hover:inset-shadow-green-500/60 transition-all duration-300 delay-75 ease-in-out" type="text" placeholder="Player 1 Name" />
+                            <input className="w-full bg-cardbg inset-shadow-sm inset-shadow-gray-100/70 px-3 py-3 text-lg font-medium rounded-md focus:outline-none hover:inset-shadow-green-500/60 transition-all duration-300 delay-75 ease-in-out" type="text" placeholder="Player 2 Name" />
+                        </div>
                 </div>
             </div>
         </div>
