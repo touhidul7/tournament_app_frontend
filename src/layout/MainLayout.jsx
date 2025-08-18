5bd164c5482c59c7acbfea55beb236957251f667
touhidul7 <touhidul2@hotmail.com>
2025-08-19 03:05:06 +0600

    update

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Outlet } from 'react-router';
 import BottomNav from '../components/BottomNav';
 import toast, { Toaster } from 'react-hot-toast';
